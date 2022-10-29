07a8d9e83cfc240fdbfc16fb680250e1f1015321
Alejandro correa Uribe <alejandrolol153@gmail.com>
2022-10-29 14:43:05 -0500

    comit
    segunda interfaz de las preguntas
    29/10/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './paginas/formulario/formulario.component';
+import { ResultadosComponent } from './paginas/resultados/resultados.component';
 
 const routes: Routes = [
     {path: '', redirectTo:'formulario' , pathMatch:'full' },
     {path:'formulario', component:FormularioComponent},
+    {
+      path:'resultados', 
+      component:ResultadosComponent,
+      children:[]
+    },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './paginas/formulario/formulario.component';
+import { ResultadosComponent } from './paginas/resultados/resultados.component';
+import { DatosUsuarioComponent } from './paginas/datos-usuario/datos-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    ResultadosComponent,
+    DatosUsuarioComponent
   ],
   imports: [
     BrowserModule,
